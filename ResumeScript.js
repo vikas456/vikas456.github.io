12daf8ce0b4463085ece640fffeb5726ff5cae0c
vikas.peraka <vikasperaka@Vikass-Air.home>
2017-12-22 18:49:13 -0600

    Updated Resume

---
+++ ResumeScript.js
@@ -0,0 +1,15 @@
+function showCiber() {
+    document.getElementById("ciberInfo").style.visibility = "visible";
+}
+
+function hideCiber() {
+    document.getElementById("ciberInfo").style.visibility = "hidden";
+}
+
+function showSchool() {
+    document.getElementById("utInfo").style.visibility = "visible";
+}
+
+function hideSchool() {
+    document.getElementById("utInfo").style.visibility = "hidden";
+}
